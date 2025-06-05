e0a517c3081beb7a306d5454f5863eaa6f20ad1a
Krishnaa-Gopi1 <krishnaagopi.dev@gmail.com>
2025-06-05 21:31:34 +0530

    till long strangle

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 from plot import plot_payoff_curve
-from payoff import long_call_payoff,long_put_payoff,bull_call_spread_payoff
+from payoff import long_call_payoff,long_put_payoff,bull_call_spread_payoff,bear_put_spread_payoff,long_straddle_payoff,long_strangle_payoff
 
-choice = int(input("choose an option strategy\n1. long call\n2. long put \n3. bull call spread\n : "))
+choice = int(input("choose an option strategy\n1. long call\n2. long put \n3. bull call spread\n4. bear put spread payoff\n5. Long straddle\n6. Long Strangle \n: "))
 
 
 stock_prices = np.arange(50,151,1)
@@ -28,6 +28,34 @@ elif choice == 3:
 
     payoff = bull_call_spread_payoff(stock_prices, lower_strike, lower_premium, upper_strike, upper_premium)
     title = "Bull Call Spread Payoff"
+
+elif choice == 4:
+    lower_strike = float(input("Enter the LOWER strike price: "))
+    lower_premium = float(input("Enter the premium PAID for the LOWER strike call: "))
+    upper_strike = float(input("Enter the UPPER strike price: "))
+    upper_premium = float(input("Enter the premium RECEIVED for the UPPER strike call: "))
+
+    payoff = bear_put_spread_payoff(stock_prices, lower_strike, lower_premium, upper_strike, upper_premium)
+    title = "Bear put Spread Payoff"
+
+elif choice == 5:
+    strike_price = float(input("Enter the strike price: "))
+    call_premium = float(input("Enter the call premium: "))
+    put_premium = float(input("Enter the put premium: "))
+    
+
+    payoff = long_straddle_payoff(stock_prices,strike_price,call_premium,put_premium)
+    title = "Long straddle payoff"
+
+elif choice == 6:
+   
+    lower_strike = float(input("Enter the LOWER strike price: "))
+    lower_premium = float(input("Enter the premium PAID for the LOWER strike call: "))
+    upper_strike = float(input("Enter the UPPER strike price: "))
+    upper_premium = float(input("Enter the premium RECEIVED for the UPPER strike call: "))
+
+    payoff = long_strangle_payoff(stock_prices,lower_strike,lower_premium,upper_strike,upper_premium)
+    title = "Long strangle payoff"
 
 else:
     print("Invalid choice")
--- payoff.py
+++ payoff.py
@@ -12,3 +12,23 @@ def bull_call_spread_payoff(stock_prices,lower_strike,lower_premium,upper_strike
 
     return((long_call-short_call) - (lower_premium-upper_premium))
 
+def bear_put_spread_payoff(stock_prices,lower_strike,lower_premium,upper_strike,upper_premium):
+    long_put = np.maximum(0,upper_strike-stock_prices)
+    short_put = np.maximum(0,lower_strike-stock_prices)
+
+    return((long_put-short_put)-(upper_premium-lower_premium))
+    
+
+def long_straddle_payoff(stock_prices,strike_price,call_premium,put_premium):
+    call = np.maximum(0,stock_prices-strike_price)
+    put = np.maximum(0,strike_price-stock_prices)
+
+    return call+put - (call_premium+put_premium)
+
+def long_strangle_payoff(stock_prices,lower_strike,lower_premium,upper_strike,upper_premium):
+    call = np.maximum(0,stock_prices-upper_strike)
+    put = np.maximum(0,lower_strike-stock_prices)
+    
+    return call+put -(upper_premium+lower_premium)
+
+    
